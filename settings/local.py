efe1ecfc23a92748e6877a03f731ea3d508b228e
hyeonukkang <hyeonukkang@bitbucket.org>
2021-06-09 18:55:59 +0900

    set env settings

---
+++ settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+ALLOWED_HOSTS = ['localhost']
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'local_db',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# python manage.py runserver --settings=settings.local
