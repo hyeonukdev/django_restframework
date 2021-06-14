cf3137fd56bfd5ff958acac589b776fce6994da2
hyeonukkang <hyeonukkang@bitbucket.org>
2021-06-14 18:50:30 +0900

    fix jwt

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -9,4 +11,8 @@ urlpatterns = [
     path('student/', include('student.urls')),
     path('blog/', include('blog.urls')),
     path('jwt/', include('blog.urls')),
+
+    path('api/token/',obtain_jwt_token),
+    path('api/token/verify/', verify_jwt_token),
+    path('api/token/refresh/', refresh_jwt_token),
 ]
